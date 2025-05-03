0f934ada819fc7027dd17fcb4923d535afccc31b
Shaik Mahammad Gouse <59640213+shaik-byte@users.noreply.github.com>
2025-05-03 10:57:28 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,41 @@
+// // karma.conf.js
+// module.exports = function (config) {
+//   config.set({
+//     basePath: '',
+//     frameworks: ['jasmine', '@angular-devkit/build-angular'],
+//     plugins: [
+//       require('karma-jasmine'),
+//       require('karma-chrome-launcher'),
+//       require('karma-jasmine-html-reporter'),
+//       require('karma-coverage'),
+//       require('@angular-devkit/build-angular/plugins/karma')
+//     ],
+//     client: {
+//       jasmine: {},
+//       clearContext: false // Leave Jasmine Spec Runner output visible in browser
+//     },
+//     coverageReporter: {
+//       dir: require('path').join(__dirname, './coverage'),
+//       subdir: '.',
+//       reporters: [
+//         { type: 'html' }, // Generates HTML report
+//         { type: 'text-summary' }, // Outputs a summary to the console
+//         { type: 'lcovonly', file: 'lcov-report/lcov.info' }, // LCOV for SonarQube
+//         { type: 'cobertura', file: 'cobertura.xml' } // Cobertura XML for SonarQube
+//       ]
+//     },
+//     reporters: ['progress', 'kjhtml', 'coverage'],
+//     port: 9876,
+//     colors: true,
+//     logLevel: config.LOG_INFO,
+//     autoWatch: false,
+//     browsers: ['ChromeHeadless'],
+//     singleRun: true,
+//     restartOnFileChange: true
+//   });
+// };
+
+
 // karma.conf.js
 module.exports = function (config) {
   config.set({
@@ -18,13 +56,10 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
-        { type: 'html' }, // Generates HTML report
-        { type: 'text-summary' }, // Outputs a summary to the console
-        { type: 'lcovonly', file: 'lcov-report/lcov.info' }, // LCOV for SonarQube
-        { type: 'cobertura', file: 'cobertura.xml' } // Cobertura XML for SonarQube
+        { type: 'lcovonly', file: 'lcov.info' } // Generates only LCOV report
       ]
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
